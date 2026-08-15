9b8b5b607c8717e091d8225c28ccee41e0b620ec
Phani Kumar Adabala <adabala.phani@gmail.com>
2026-08-15 16:20:45 +0530

    fix(ui): fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.adabala.sandeshika"
         minSdk = 27
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.6.0"
+        versionCode = 11
+        versionName = "1.7.0"
     }
 
     signingConfigs {
